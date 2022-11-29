5682ff07ce4ad192c83b25a341fa684a5d5751cb
Mason <Mason0015s@gmail.com>
2022-11-28 21:11:13 -0500

    Fixes

---
+++ Source/CoffeePointOfSale/Services/Order/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePointOfSale.Services.Order {
+    public class Order {
+        private List<Drink.Drink> drinks;
+        private decimal tax;
+        private decimal subtotal;
+        private decimal total;
+        private string paymentMethod;
+        private Customer.Customer customer;
+
+        public Order() {
+            drinks = new List<Drink.Drink>();
+            tax = new decimal(0.00);
+            subtotal = new decimal(0.00);
+            total = new decimal(0.00);
+            paymentMethod = "";
+        }
+
+        public decimal Tax { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Total { get; set; }
+        public string PaymentMethod { get; set; }
+        public CoffeePointOfSale.Services.Customer.Customer Customer { get; set; }
+
+        public void AddDrink(Drink.Drink newDrink) {
+            drinks.Add(newDrink);
+        }
+    }
+}
---
+++ Source/CoffeePointOfSale/Services/Payment/PaymentHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePointOfSale.Services.Payment {
+    public class PaymentHandler {
+        private Order.Order currentOrder;
+        private Form paymentScreenForm;
+
+        public PaymentHandler() {
+
+        }
+
+        public void GoToReceipt(bool withCard) {
+            if (withCard) {
+                GetCardNumber();
+            }
+            else {
+                //look at reward points
+            }
+            //share data and go to receipt form
+        }
+
+        public void SetCurrentOrder(Order.Order currentOrder) {
+            this.currentOrder = currentOrder;
+        }
+
+        public void SetPaymentScreenForm(Form paymentScreenForm) {
+            this.paymentScreenForm = paymentScreenForm;
+        }
+
+        public void CancelOrder() {
+            //go back to FormMain
+        }
+
+        private void DisplayCustomerName() {
+            //display customer name on form element
+        }
+
+        private void DisplayTotal() {
+            //display Total on form element
+        }
+
+        private string GetCardNumber() {
+            string cardNumber = "";
+
+            //get card number from input element on form
+
+            if (ValidateCardNumber(cardNumber)) {
+                return cardNumber;
+            }
+            else 
+            {
+                throw new ArgumentException("Invalid Card Number");
+            }
+            
+        }
+
+        private bool ValidateCardNumber(string cardNumber) {
+            bool valid = false;
+
+            //validate card number using math
+
+            return valid;
+        }
+
+        private int CalculateNeededRewardPoints() {
+            int neededPoints = (int)currentOrder.Total * 10;
+            return neededPoints;
+        }
+
+        private bool CheckRewardPoints() {
+            int neededPoints = CalculateNeededRewardPoints();
+            if (neededPoints <= currentOrder.Customer.RewardPoints) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+
+    }
+}

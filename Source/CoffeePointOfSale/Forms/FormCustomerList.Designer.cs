0c9eb98f23ea173c1c3faac276437989c30f45da
Joel <joelj1224@gmail.com>
2022-11-29 01:34:20 -0500

    update

--- Source/CoffeePointOfSale/Forms/FormCustomerList.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormCustomerList.Designer.cs
@@ -70,7 +70,7 @@
             this.listBox1.ItemHeight = 45;
             this.listBox1.Location = new System.Drawing.Point(79, 24);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1176, 495);
+            this.listBox1.Size = new System.Drawing.Size(1176, 499);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 

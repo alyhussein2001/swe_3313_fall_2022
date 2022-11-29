ddb5335d7d785f107691c4be820828568f54c1e9
Mason <Mason0015s@gmail.com>
2022-11-29 01:02:27 -0500

    New Stuff

--- Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenu.cs
+++ Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenu.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+
 
 namespace CoffeePointOfSale.Services.DrinkMenu {
-    internal class DrinkMenu {
+    public class DrinkMenu {
+        [JsonProperty("DrinkMenu")]
+        private readonly Dictionary<string, Drink> _drinkDict = new();
+
+        [JsonIgnore] //not written to JSON file... this is a list view of the dictionary, so only the dictionary is written
+        public IReadOnlyList<Drink> List =>
+        _drinkDict.Select(c => c.Value)
+            .ToList();
+
 
     }
 }

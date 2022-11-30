1de1fcc9c69126154e6e30fc05a069e713c50f6e
Joel <joelj1224@gmail.com>
2022-11-29 23:25:48 -0500

    FormManagement FIXED HURRAH!

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -29,41 +29,35 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
-            this.txtDeleteThis = new System.Windows.Forms.TextBox();
-            this.lblRemoveThis = new System.Windows.Forms.Label();
+            this.bCSV = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(164, 26);
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Location = new System.Drawing.Point(416, 375);
             this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(82, 22);
+            this.btnClose.Size = new System.Drawing.Size(438, 78);
             this.btnClose.TabIndex = 0;
-            this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Text = "Main Menu";
+            this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.OnClickBtnClose);
             // 
-            // txtDeleteThis
+            // bCSV
             // 
-            this.txtDeleteThis.AcceptsReturn = true;
-            this.txtDeleteThis.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtDeleteThis.Location = new System.Drawing.Point(164, 82);
-            this.txtDeleteThis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtDeleteThis.Multiline = true;
-            this.txtDeleteThis.Name = "txtDeleteThis";
-            this.txtDeleteThis.Size = new System.Drawing.Size(482, 76);
-            this.txtDeleteThis.TabIndex = 1;
-            this.txtDeleteThis.TextChanged += new System.EventHandler(this.txtDeleteThis_TextChanged);
-            // 
-            // lblRemoveThis
-            // 
-            this.lblRemoveThis.AutoSize = true;
-            this.lblRemoveThis.Location = new System.Drawing.Point(164, 65);
-            this.lblRemoveThis.Name = "lblRemoveThis";
-            this.lblRemoveThis.Size = new System.Drawing.Size(140, 15);
-            this.lblRemoveThis.TabIndex = 2;
-            this.lblRemoveThis.Text = "Demo - Remove This Box";
+            this.bCSV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.bCSV.Font = new System.Drawing.Font("Century Gothic", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bCSV.ForeColor = System.Drawing.Color.Transparent;
+            this.bCSV.Location = new System.Drawing.Point(323, 196);
+            this.bCSV.Name = "bCSV";
+            this.bCSV.Size = new System.Drawing.Size(639, 75);
+            this.bCSV.TabIndex = 1;
+            this.bCSV.Text = "Download CSV Sales Data File";
+            this.bCSV.UseVisualStyleBackColor = false;
+            this.bCSV.Click += new System.EventHandler(this.bCSV_Click);
             // 
             // FormManagement
             // 
@@ -71,23 +65,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(223)))), ((int)(((byte)(225)))));
             this.ClientSize = new System.Drawing.Size(1332, 682);
-            this.Controls.Add(this.lblRemoveThis);
-            this.Controls.Add(this.txtDeleteThis);
+            this.Controls.Add(this.bCSV);
             this.Controls.Add(this.btnClose);
-            this.Enabled = false;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormManagement";
             this.Text = "FormManagement";
             this.Load += new System.EventHandler(this.OnLoadFormManagement);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button btnClose;
-        private TextBox txtDeleteThis;
-        private Label lblRemoveThis;
+        private Button bCSV;
     }
 }
